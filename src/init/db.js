957c85b1c4fd22a177bd88e316d30011d0ced270
woaitsAryan <yoprince.ab@gmail.com>
2023-09-20 15:01:51 +0530

    small changes

--- src/init/db.js
+++ src/init/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-// const URL = "mongodb://mongo-db:27018";
-const URL = "mongodb://localhost:27017";
+const URL = "mongodb://mongo-db:27018";
+// const URL = "mongodb://localhost:27017";
 const connectToDB = () =>
     mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connected to Database!'));
 
